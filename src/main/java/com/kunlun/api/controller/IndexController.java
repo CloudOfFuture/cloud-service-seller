312b5412f5a49e09948916b5e63d2b84f115507f
Jarven <769558579@qq.com>
2017-12-26 10:01:29 +0800

    取消分布式事务 暂时先不使用事务

---
+++ src/main/java/com/kunlun/api/controller/IndexController.java
@@ -0,0 +1,14 @@
+package com.kunlun.api.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author by kunlun
+ * @version <0.1>
+ * @created on 2017/12/25.
+ */
+@RestController
+@RequestMapping("")
+public class IndexController {
+}
